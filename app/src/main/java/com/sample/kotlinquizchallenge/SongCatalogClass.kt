8d8c76c960375ebfe18cac6af474c3915f8b1027
Madhan Mohan Gangadhari <madhanmohangangadhari@Madhans-MacBook-Pro.local>
2025-10-15 15:39:16 -0400

    [TASK] Finished SongCatalogClass Question

--- app/src/main/java/com/sample/kotlinquizchallenge/SongCatalogClass.kt
+++ app/src/main/java/com/sample/kotlinquizchallenge/SongCatalogClass.kt
@@ -14,5 +14,27 @@ package com.sample.kotlinquizchallenge
 * */
 
 fun main() {
+    val song = Song("Baby Baby", "J Bieber", 2_500_000, 354)
+    println("Title: ${song.title}")
+    println("Artist: ${song.artist}")
+    println("Duration: ${song.formattedDuration}")
+    println("Popular: ${song.isPopular()}")
+}
 
+class Song(
+    val title: String,
+    val artist: String,
+    val playCount: Int,
+    val durationInSeconds: Int
+) {
+    val formattedDuration: String
+        get() {
+            val minutes = durationInSeconds / 60
+            val seconds = durationInSeconds % 60
+            return String.format("%02d:%02d", minutes, seconds)
+        }
+}
+
+fun Song.isPopular(): Boolean {
+    return playCount > 1_000_000
 }
